80061d3d4b22df4565258926a51395a7b87fbe38
mumoshu <ykuoka@gmail.com>
2013-07-20 18:51:59 +0900

    Refactor to use the value instead of a literal

--- play2-example/app/controllers/ProtectedResourcesController.scala
+++ play2-example/app/controllers/ProtectedResourcesController.scala
@@ -2,13 +2,14 @@ package controllers
 
 import play.api.mvc._
 import models.Token
+import oauth2.AuthorizationHeader
 
 object ProtectedResourcesController extends Controller {
 
   def get = Action { implicit request =>
     // 7. Accessing Protected Resources
     // http://tools.ietf.org/html/draft-ietf-oauth-v2-31#section-7
-    request.headers.get("Authorization").map(_.split(" ")) match {
+    request.headers.get(AuthorizationHeader.Name).map(_.split(" ")) match {
       case Some(Array("Bearer", accessToken)) =>
         Token.find(accessToken) match {
           case Some(token) if !token.isExpired =>
