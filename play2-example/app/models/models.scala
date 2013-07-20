6bff7b10cbbc1db9dc0fcc884bed574aafe8b029
mumoshu <ykuoka@gmail.com>
2013-07-20 18:52:32 +0900

    Add some comment to show which characters are allowed in Scopes

--- play2-example/app/models/models.scala
+++ play2-example/app/models/models.scala
@@ -7,6 +7,11 @@ import java.net.{URLDecoder, URLEncoder, URI}
 /**
  * Scope of authorizations to take control of accesses to protected resources
  *
+ * Valid characters for scopes are as follows:
+ *
+ * scala> 0x23 :: (0x23 to 0x5b toList) ++ (0x5d to 0x7e) map (_.toChar) mkString (" ")
+ * res28: String = # # $ % & ' ( ) * + , - . / 0 1 2 3 4 5 6 7 8 9 : ; < = > ? @ A B C D E F G H I J K L M N O P Q R S T U V W X Y Z [ ] ^ _ ` a b c d e f g h i j k l m n o p q r s t u v w x y z { | } ~
+ *
  * @param value
  * @param scope
  * @see http://tools.ietf.org/html/draft-ietf-oauth-v2-31#section-3.3
